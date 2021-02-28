e711504eff855b8ee6be15d605faee468e6f00d3
vladiolus3 <vladyslav.dovhal@nure.ua>
2021-03-01 00:46:24 +0300

    hw10

---
+++ HW10/Task_1/Controller/PrimesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Task_1.Controller
+{
+    [ApiController]
+    [Route("primes")]
+    public class PrimesController : ControllerBase
+    {
+        private readonly ILogger<Startup> _logger;
+
+        public PrimesController(ILogger<Startup> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{number:int}")]
+        public async Task<IActionResult> IsPrime([FromRoute] int number)
+        {
+            _logger.LogInformation("The number is checked for simplicity");
+            if (number > 1 && await PrimeNumberFinder.IsPrime(number))
+            {
+                _logger.LogInformation($"The number {number} is prime");
+                return Ok();
+            }
+
+            _logger.LogInformation($"The number {number} is not prime");
+            return NotFound($"The number {number} is not prime");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PrimeRange([FromQuery] int from, [FromQuery] int to)
+        {
+            _logger.LogInformation("The range is checked for simplicity");
+            if (!HttpContext.Request.Query.ContainsKey("from") ||
+                    !HttpContext.Request.Query.ContainsKey("to") ||
+                     !int.TryParse(HttpContext.Request.Query["from"].FirstOrDefault(), out _) ||
+                     !int.TryParse(HttpContext.Request.Query["to"].FirstOrDefault(), out _)
+            )
+            {
+                _logger.LogInformation("Not enough correct parameters");
+                return BadRequest("Not enough correct parameters");
+            }
+
+            var json = JsonSerializer.Serialize(await PrimeNumberFinder.PrimeRange(from, to));
+            _logger.LogInformation("Search for prime numbers in the range is over");
+            return Content(json);
+        }
+    }
+}
---
+++ HW10/Task_2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var path = "address.json";
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Not found correct base address");
+                return;
+            }
+
+            string url;
+            try
+            {
+                var text = File.ReadAllText(path);
+                url = JsonSerializer.Deserialize<string>(text);
+            }
+            catch
+            {
+                Console.WriteLine("Cou1d not read the contents of the file");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(url))
+            {
+                try
+                {
+                    using var client = new HttpClient()
+                    {
+                        BaseAddress = new Uri(url)
+                    };
+                    await TestInfo(client);
+                    await TestNumberIsPrime(client);
+                    await TestNumberIsNotPrime(client);
+                    await TestCorrectRange(client);
+                    await TestEmptyRange(client);
+                    await TestIncorrectRange(client);
+                }
+                catch (HttpRequestException)
+                {
+                    Console.WriteLine("Failed to create request");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Found url is null");
+            }
+
+            Console.ReadKey();
+        }
+
+        static async Task TestInfo(HttpClient client)
+        {
+            Console.WriteLine("GET http://localhost:5000/");
+            var response = await client.GetAsync("");
+            var text = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Received Body:\t" + text);
+            Console.WriteLine("Expected Body:\t\"Prime numbers\" by st. Dovhal Vladyslav");
+            Console.WriteLine("Expected Status:\t" + HttpStatusCode.OK);
+            Console.WriteLine("Received Status:\t" + response.StatusCode);
+            Debug.Assert(response.StatusCode == HttpStatusCode.OK && text == "Prime numbers\" by st. Dovhal Vladyslav");
+            Console.WriteLine("Test finished!\n\n");
+        }
+
+        static async Task TestNumberIsPrime(HttpClient client)
+        {
+            Console.WriteLine("GET http://localhost:5000/primes/5");
+            var response = await client.GetAsync("/primes/5");
+            Console.WriteLine("Expected Status:\t" + HttpStatusCode.OK);
+            Console.WriteLine("Received Status:\t" + response.StatusCode);
+            Debug.Assert(response.StatusCode == HttpStatusCode.OK);
+            Console.WriteLine("Test finished!\n\n");
+        }
+
+        static async Task TestNumberIsNotPrime(HttpClient client)
+        {
+            Console.WriteLine("GET http://localhost:5000/primes/4");
+            var response = await client.GetAsync("/primes/4");
+            Console.WriteLine("Expected Status:\t" + HttpStatusCode.NotFound);
+            Console.WriteLine("Received Status:\t" + response.StatusCode);
+            Debug.Assert(response.StatusCode == HttpStatusCode.NotFound);
+            Console.WriteLine("Test finished!\n\n");
+        }
+
+        static async Task TestCorrectRange(HttpClient client)
+        {
+            Console.WriteLine("GET http://localhost:5000/primes?from=2&to=5");
+            var response = await client.GetAsync("/primes?from=2&to=5");
+
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Received Body:\t" + body);
+            Console.WriteLine("Expected Body:\t[2,3,5]");
+            Console.WriteLine("Expected Status:\t" + HttpStatusCode.OK);
+            Console.WriteLine("Received Status:\t" + response.StatusCode);
+            Debug.Assert(body == "[2,3,5]" && response.StatusCode == HttpStatusCode.OK);
+            Console.WriteLine("Test finished!\n\n");
+        }
+
+        static async Task TestEmptyRange(HttpClient client)
+        {
+            Console.WriteLine("GET http://localhost:5000/primes?from=-5&to=1");
+            var response = await client.GetAsync("/primes?from=-5&to=1");
+
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Received Body:\t" + body);
+            Console.WriteLine("Expected Body:\t[]");
+            Console.WriteLine("Expected Status:\t" + HttpStatusCode.OK);
+            Console.WriteLine("Received Status:\t" + response.StatusCode);
+            Debug.Assert(body == "[]" && response.StatusCode == HttpStatusCode.OK);
+            Console.WriteLine("Test finished!\n\n");
+        }
+
+        static async Task TestIncorrectRange(HttpClient client)
+        {
+            Console.WriteLine("GET http://localhost:5000/primes?to=abcd");
+            var response = await client.GetAsync("/primes?to=abcd");
+            Console.WriteLine("Expected Status:\t" + HttpStatusCode.BadRequest);
+            Console.WriteLine("Received Status:\t" + response.StatusCode);
+            Debug.Assert(response.StatusCode == HttpStatusCode.BadRequest);
+            Console.WriteLine("Test finished!\n\n");
+        }
+    }
+}
